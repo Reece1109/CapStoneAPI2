90ea784dc8ab50fb8cee93e3aa30ff491d392984
Reece1109 <reecesimon84@gmail.com>
2023-03-13 09:06:42 +0200

    Update

---
+++ api/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+
+// route
+const route = require('./controllers/index');
+
+// cors
+const cors = require('cors');
+
+// port
+const port = parseInt(process.env.PORT) || 4000;
+
+// Express app
+const app = express();
+
+// Middleware
+// const errorHandling = require('./middleware/ErrorHandling');
+
+//
+const cookieParser = require('cookie-parser');
+
+/*
+express.json: setting the content-type to application/json
+bodyParser.urlencoded( {extended: true} ): The Object will contain
+values of any type instead of just a string
+*/
+
+app.use((req, res, next)=> {
+        res.header('Access-Control-Allow-Origin', 'https://sushi-store-c3c59.web.app/')
+        res.header("Access-Control-Allow-Credentials", "true")
+        res.header("Access-Control-Allow-Methods", "*")
+        res.header("Access-Control-Allow-Headers", "*")
+        next();
+});
+app.use(route);
+app.use(
+    cors(),
+    cookieParser(),
+    express.json(),
+    express.urlencoded({extended: false})
+)
+
+
+// The Server is running
+app.listen(port, ()=> {
+    console.log(`Server is running on ${port}`)
+});
+// The Handling of all errors
+// app.use(errorHandling);
